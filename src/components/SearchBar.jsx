7f495752fee3bc0291196a4cf39693ce0010468c
Brayan Steven Narvaez Valdes <brayan.steven.narvaez@correounivalle.edu.co>
2024-07-23 21:06:45 -0500

    d

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,27 +3,20 @@ import React from "react";
 
 export default function SearchBar({ characterSearch, setCharacterSearch }) {
   return (
-    <div className="w-full mb-4 py-2 pb-6 mx-auto">
-      <div
-        id="search-bar"
-        className="w-120 bg-white rounded-md shadow-xl z-10"
-      >
+    <div className="mx-auto mb-4 w-full py-2 pb-6">
+      <div id="search-bar" className="w-120 z-10 rounded-md bg-white shadow-xl">
         <form className="flex items-center justify-center p-2">
           <input
             type="text"
             placeholder="Search here"
-            className="w-full rounded-md px-2 py-1 
-            focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent
-            "
+            className="w-full rounded-md px-2 py-1 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-gray-600"
             value={characterSearch}
             name={characterSearch}
             onChange={(e) => setCharacterSearch(e.target.value)}
           />
           <button
             type="submit"
-            className="bg-gray-800 text-white rounded-md px-4 py-1 ml-2
-             hover:bg-gray-700 
-             focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
+            className="ml-2 rounded-md bg-gray-800 px-4 py-1 text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50"
           >
             Search
           </button>
