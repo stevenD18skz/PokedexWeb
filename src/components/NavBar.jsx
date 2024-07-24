864d8806957b8af3bee6c374d359c94d966fa5b9
Brayan Steven Narvaez Valdes <brayan.steven.narvaez@correounivalle.edu.co>
2024-07-24 13:49:36 -0500

    navBar completa

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,7 +14,7 @@ export default function NavBar() {
   return (
     <nav className="fixed top-0 z-50 w-full bg-gray-900 p-4 shadow-lg transition-all duration-300 ease-in-out">
       <div className="flex items-center justify-between">
-        <div className="text-5xl font-bold text-white">
+        <div className="text-2xl font-bold text-white md:text-5xl">
           <a href="/">PokeDex</a>
         </div>
 
@@ -23,7 +23,7 @@ export default function NavBar() {
             {dataBar.map((op, index) => (
               <li key={index}>
                 <a href={op[2]} className="block">
-                  <div className="flex flex-col items-center text-center text-white hover:bg-gray-500 hover:text-gray-400">
+                  <div className="hover:bg-secondary hover:text-primary flex flex-col items-center text-center text-white">
                     <p className="text-lg">{op[0]}</p>
                     <p className="text-md">{op[1]}</p>
                   </div>
@@ -33,22 +33,21 @@ export default function NavBar() {
           </ul>
         </div>
 
-        <div className="relative">
-          <button onClick={toggleMenu} className="text-5xl text-white">
+        <div className="relative md:hidden">
+          <button onClick={toggleMenu} className="text-2xl text-white">
             {isOpen ? "✖" : "☰"}
           </button>
 
-          {/* Menu desplegable */}
           <ul
-            className={`absolute right-8 top-full mt-1 w-48 rounded-lg bg-gray-700 text-white shadow-lg transition-transform duration-300 ${
+            className={`absolute right-8 top-full mt-1 w-48 rounded-lg bg-slate-800 text-white shadow-lg transition-transform duration-300 ${
               isOpen ? "scale-100" : "scale-0"
             } origin-top-right transform`}
           >
             {dataBar.map((op, index) => (
               <li key={index}>
                 <a href={op[2]} className="block p-2">
-                  <div className="flex flex-col rounded-md text-center hover:bg-gray-500">
-                    <p className="text-xl">{op[0]}</p>
+                  <div className="hover:bg-primary hover:text-secondary flex flex-col rounded-md text-center">
+                    <p className="text-md">{op[0]}</p>
                     <p className="text-sm">{op[1]}</p>
                   </div>
                 </a>
