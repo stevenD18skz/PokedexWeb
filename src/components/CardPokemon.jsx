d757a0180866633e5d73c07cf7b217bb0448b502
stevenD18skz <brayanss2018@gmail.com>
2025-08-07 09:36:28 -0500

    Fix Link in CardPokemon component to use relative path for navigation

--- src/components/CardPokemon.jsx
+++ src/components/CardPokemon.jsx
@@ -14,7 +14,7 @@ export default function CardPokemon({ data }) {
     <div
       className={`w-full transform overflow-hidden rounded-3xl bg-gray-900 shadow-xl shadow-gray-500/50 transition duration-500 hover:-translate-y-3 hover:shadow-gray-600/70`}
     >
-      <Link to={`http://localhost:5173/pokedex/${data.name}`}>
+      <Link to={`/pokedex/${data.name}`}>
         <img className={difusionTailwind} src={data.image} alt={data.name} />
       </Link>
 
